7c0217c786eb507bd57f53133bc77fdbb1253bf5
alet89 <20442827+alet89@users.noreply.github.com>
2021-10-31 16:51:28 +0100

    Routines

--- src/components/Covenants/RoutineInfo/index.js
+++ src/components/Covenants/RoutineInfo/index.js
@@ -6,15 +6,19 @@ import style from './routine-info.module.css'
 import BackButton from '../../Global/BackButton/index.js'
 import RegularButton from '../../Global/RegularButton/index.js'
 import ExtLinkButton from '../../Global/ExtLinkButton/index.js'
+import RoutinesOperation from '../../Covenants/RoutinesOperation/index.js'
 
 const RoutineInfo = (props) => {
   return (
     <div className={style.RutineBox}>
       <div className={style.RutineTitle}>
-        <h2>EthOS Fixed Inflation - OS token daily</h2>
         <div className={style.RutineBack}>
           <BackButton></BackButton>
         </div>
+        <h2>EthOS Fixed Inflation - OS token daily</h2>
+      </div>
+      <div className={style.OperationsBox}>
+        <RoutinesOperation></RoutinesOperation>
       </div>
     </div>
   )
---
+++ src/components/Covenants/RoutinesOperation/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Typography } from '@ethereansos/interfaces-ui'
+
+import style from './routines-operation.module.css'
+import RegularButton from '../../Global/RegularButton/index.js'
+import ExtLinkButton from '../../Global/ExtLinkButton/index.js'
+import ItemLinkButton from '../../Global/ItemLinkButton/index.js'
+
+const RoutinesOperation = (props) => {
+  return (
+    <>
+      {/*Single Farm Start*/}
+      <div className={style.RoutineOperation}>
+        
+      </div>
+      {/*Single Farm End*/}
+    </>
+  )
+}
+
+export default RoutinesOperation
