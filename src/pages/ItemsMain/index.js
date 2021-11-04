22ff297c54ec25d13f10916af560f384a73c3862
alet89 <20442827+alet89@users.noreply.github.com>
2021-11-04 12:04:59 +0100

    ssssss

--- src/pages/ItemsMain/index.js
+++ src/pages/ItemsMain/index.js
@@ -4,6 +4,7 @@ import {Typography} from '@ethereansos/interfaces-ui'
 import style from './items-main.module.css'
 import CollectionView  from './Sections/collection-view'
 import ItemView  from './Sections/item-view'
+import ItemsExplore  from './Sections/items-explore'
 import DappMenu from './../../components/Global/DappMenu/index.js'
 
 const ItemsMain = (props) => {
