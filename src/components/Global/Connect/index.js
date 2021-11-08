675d5b97b178842db5ac11326a43e9955eac8089
Marco Vasapollo <ceo@metaring.com>
2021-11-08 19:10:25 +0100

    chore: wip

--- src/components/Global/Connect/index.js
+++ src/components/Global/Connect/index.js
@@ -1,25 +1,92 @@
-import { useEffect } from 'react'
-import { useWeb3, webs3States, usePrevious, useEthosContext } from '@ethereansos/interfaces-core'
-import { ConnectWidget } from '@ethereansos/interfaces-ui'
+import { useState } from 'react'
+import { Button, CircularProgress, Typography, Modal } from '@ethereansos/interfaces-ui'
+import { useWeb3, web3States, usePrevious, useEthosContext } from '@ethereansos/interfaces-core'
 import style from './connect.module.css'
-import ModalStandard from '../ModalStandard'
 
 const Connect = ({ children }) => {
   const context = useEthosContext()
   const { wallet, connectionStatus } = useWeb3();
 
-  return connectionStatus === webs3States.CONNECTED ? (
+  function close() {
+    window.location.href = window.location.href.split('/#')[0] + '/#/'
+  }
+
+  return connectionStatus === web3States.CONNECTED ? (
     children
   ) : (
-    <ModalStandard>
+    <Modal centered visible>
+      <Button text="X" onClick={close} style={{"float" : "right"}}/>
       <ConnectWidget
         title="Welcome Etherean"
         wallet={wallet}
         connectionStatus={connectionStatus}
         connectors={context.useWalletSettings}
       />
-    </ModalStandard>
+    </Modal>
   )
 }
 
 export default Connect
+
+const ConnectWidget = ({
+  title,
+  connectionStatus,
+  connectors,
+  wallet
+}) => {
+  const [activeConnector, setActiveConnector] = useState({
+    name: '',
+    label: '',
+  })
+
+  const onConnectorClicked = async (name, label) => {
+    wallet.connect(name)
+    setActiveConnector({ label, name })
+  }
+
+  return (
+    <div>
+      <Typography variant="h1" color="primary">
+        {title}
+      </Typography>
+      <br />
+      {connectionStatus === web3States.CONNECTED && <div>Connected</div>}
+      {connectionStatus === web3States.CONNECTING && <div>Connecting</div>}
+      {connectionStatus === web3States.NOT_CONNECTED && <Typography variant="body2" align="center">
+          You need a{' '}
+          <a target="_blank" href="https://etherscan.io/directory/Wallet">Web3 Enabler</a> to
+          use this Dapp - If you have problems connecting, refresh the page.
+        </Typography>}
+        <br/>
+        <div>
+          <Typography variant="h5">
+            Connect to a wallet
+          </Typography>
+        </div>
+        <br/>
+        {(!activeConnector.name || wallet.status === 'error') &&
+          connectors.map(connector => <>
+            <Button
+              key={connector.id}
+              text={connector.buttonText}
+              onClick={() =>
+                onConnectorClicked(connector.id, connector.buttonText)
+              }
+            />
+            <br/>
+            <br/>
+            </>
+          )}
+          <br/>
+          <br/>
+          {wallet.status === 'error' &&
+            <Typography variant="body1">{wallet.error?.message}</Typography>
+          }
+          {wallet.status === 'connecting' &&
+            <Typography variant="body1">
+              Connecting to {activeConnector.label} <CircularProgress />
+            </Typography>
+          }
+    </div>
+  )
+}
--- src/components/Global/Web3Connect/index.js
+++ src/components/Global/Web3Connect/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react'
 import style from './web3-connect.module.css'
-import { useWeb3, webs3States, truncatedWord, useEthosContext } from '@ethereansos/interfaces-core'
+import { useWeb3, web3States, truncatedWord, useEthosContext } from '@ethereansos/interfaces-core'
 
 const Web3Connect = () => {
   const context = useEthosContext()
   const { account, connectionStatus, disconnect } = useWeb3();
   function triggerConnect() {
     var location = window.location.href.toString();
-    connectionStatus === webs3States.NOT_CONNECTED && (window.location.href = (location.lastIndexOf('/') === location.length - 1 ? location.substring(0, location.length - 1) : location) + '/dapp');
-    connectionStatus === webs3States.CONNECTED && disconnect();
+    connectionStatus === web3States.NOT_CONNECTED && (window.location.href = (location.lastIndexOf('/') === location.length - 1 ? location.substring(0, location.length - 1) : location) + '/dapp');
+    connectionStatus === web3States.CONNECTED && disconnect();
   }
 
   return (
@@ -20,7 +20,7 @@ const Web3Connect = () => {
         </a>*/}
         <a href="javascript:;" onClick={triggerConnect} className={style.Web3ConnectWallet}>
           <img src={`${process.env.PUBLIC_URL}/img/ethereum.png`}></img>
-          <p>{connectionStatus === webs3States.NOT_CONNECTED ? "Connect" : truncatedWord({context, charsAmount : 5 }, account)}</p>
+          <p>{connectionStatus === web3States.NOT_CONNECTED ? "Connect" : truncatedWord({context, charsAmount : 5 }, account)}</p>
         </a>
       </div>
   )
