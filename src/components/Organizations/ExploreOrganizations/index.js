eebf16b51e1dfe0c31168eae5cf68a1c7741a650
alet89 <20442827+alet89@users.noreply.github.com>
2021-11-10 11:15:56 +0100

    a

--- src/components/Organizations/ExploreOrganizations/index.js
+++ src/components/Organizations/ExploreOrganizations/index.js
@@ -15,22 +15,66 @@ const ExploreOrganizations = (props) => {
         <div className={style.OrgTitle}>
           <h6>BojackSwap DAO</h6>
           <div className={style.OrgTitleAside}>
-            
+            <p>Functions:</p>
+            <p>26</p>
+          </div>
+          <div className={style.OrgTitleAsideB}>
+            <p>Governance:</p>
+            <p>10</p>
           </div>
         </div>
-        <div className={style.ItemInfo}>
-          <div className={style.ItemInfoSide}>
-            <p>Supply:</p>
-            <p>1,000,000</p>
-          </div>
-          <div className={style.ItemInfoSide2}>
-            <p>Price:</p>
-            <p>$13</p>
-          </div>
-        </div>
+        
      </div>
      {/*Single Item End*/}
-    
+     <div className={style.OrgSingle}>
+        <figure>
+          <img src={`${process.env.PUBLIC_URL}/img/test.jpg`}></img>
+        </figure>
+        <div className={style.OrgTitle}>
+          <h6>BojackSwap DAO</h6>
+          <div className={style.OrgTitleAside}>
+            <p>Functions:</p>
+            <p>26</p>
+          </div>
+          <div className={style.OrgTitleAsideB}>
+            <p>Governance:</p>
+            <p>10</p>
+          </div>
+        </div>
+        
+     </div><div className={style.OrgSingle}>
+        <figure>
+          <img src={`${process.env.PUBLIC_URL}/img/test.jpg`}></img>
+        </figure>
+        <div className={style.OrgTitle}>
+          <h6>BojackSwap DAO</h6>
+          <div className={style.OrgTitleAside}>
+            <p>Functions:</p>
+            <p>26</p>
+          </div>
+          <div className={style.OrgTitleAsideB}>
+            <p>Governance:</p>
+            <p>10</p>
+          </div>
+        </div>
+        
+     </div><div className={style.OrgSingle}>
+        <figure>
+          <img src={`${process.env.PUBLIC_URL}/img/test.jpg`}></img>
+        </figure>
+        <div className={style.OrgTitle}>
+          <h6>BojackSwap DAO</h6>
+          <div className={style.OrgTitleAside}>
+            <p>Functions:</p>
+            <p>26</p>
+          </div>
+          <div className={style.OrgTitleAsideB}>
+            <p>Governance:</p>
+            <p>10</p>
+          </div>
+        </div>
+        
+     </div>
      </div>
   )
 }
