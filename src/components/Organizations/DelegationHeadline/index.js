25e9a1a100f0c8f281f95606e820c1603d289f9f
alet89 <20442827+alet89@users.noreply.github.com>
2021-11-27 17:47:18 +0100

    ddddd

--- src/components/Organizations/DelegationHeadline/index.js
+++ src/components/Organizations/DelegationHeadline/index.js
@@ -7,24 +7,22 @@ import ExtLinkButton from '../../Global/ExtLinkButton/index.js'
 
 const DelegationHeadline = ({element}) => {
   return (
-    <div className={style.DelegationHeadlineCardUP}>
-      <div className={style.DelegationHeadlineCard}>
+    <div className={style.DelegationHeadlineCardUPD}>
+      <div className={style.DelegationMainThingsCardD}>
           <LogoRenderer input={element}/>
-          <div className={style.OrgTitle}>
+          <div className={style.DelLinks}>
             <h6>{element.name}</h6>
             <ExtLinkButton></ExtLinkButton>
             <ExtLinkButton></ExtLinkButton>
-            <ExtLinkButton></ExtLinkButton>
-            <ExtLinkButton></ExtLinkButton>
           </div>
-          <div className={style.DelegationHeadlineCardSide}>
+          <div className={style.DelegationHeadlineCardSideD}>
             <p><b>Created:</b> <a>32542555</a> <b>Core:</b>v. <a>1.0</a></p>
           </div>
-        </div>
-        <div className={style.ViewDescription}>
+      </div>
+        <div className={style.ViewDescriptionD}>
         <p>Welcome to the Buidlerberg Event 2020! Tucked away deep inside the Ethereum network, a community of researchers study and experiment with DFO technology. They are developing the first genuinely on-chain organizations of the future, together with taking on the task of buidling a genuinely decentralized dApp ecosystem. All researchers are invited to share their projects at the first-ever Buidlerberg meeting. more info:</p>
       </div>
-      <div className={style.DelegationMainThingsCardMainInfo}>
+      <div className={style.DelegationMainThingsCardMainInfoD}>
         <p><b>Type</b><br></br>Poll</p>
         <p><b>Grants</b><br></br>10</p>
         <p><b>Tot Funds</b><br></br>250 ETH</p>
