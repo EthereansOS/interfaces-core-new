58158f95967197fdd05d33a2be0b5d6d8cde7dd8
alet89 <20442827+alet89@users.noreply.github.com>
2021-11-28 23:10:37 +0100

    gggg

--- src/components/Global/BetaBanner/index.js
+++ src/components/Global/BetaBanner/index.js
@@ -9,10 +9,11 @@ const BetaBanner = ({close}) => {
                 <h5><b>The EthOS interface is in BETA, and has limited features.</b></h5>
                 <h6>We'll introduce these over the next few weeks. Currently missing are:</h6>
                 <p>Create an Organization</p>
-                <p>Create an Item</p>
+                <p>Create or View an Item</p>
                 <p>Create or View a Factory</p>
-                <p>Create a Farming or a Routine contract</p>
-                <h6>If you find any issues, or have cool ideas to share, please contact us on GitHub here: <a target="_blank" ></a></h6>
+                <p>Create or view Covenants</p>
+                <p>Mobile View and Dark Mode</p>
+                <h6>If you find any issues, or have cool ideas to share, please contact us <a target="_blank" href="https://discord.gg/5YNX74qNYj">here</a></h6>
             <a className={style.Enter} onClick={close}>Enter</a>
 
             </div>
