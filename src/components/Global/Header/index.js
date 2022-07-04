27530002f5f1f75e028471309d9bbc300fabebe9
alet89 <20442827+alet89@users.noreply.github.com>
2022-07-05 00:20:19 +0200

    Optimism button

--- src/components/Global/Header/index.js
+++ src/components/Global/Header/index.js
@@ -16,12 +16,26 @@ const Header = (props) => {
         <div className={style.FixedHeader}>
           <Link to="/dapp" className={style.logoMain}><img src={`${process.env.PUBLIC_URL}/img/logo_main.png`}/></Link>
           <Navigation menuName={props.menuName} isDapp={props.isDapp} selected={props.link}/>
-          <Web3Connect/>
-          <div className={style.ThemeSelect}>
-            <select value={theme} onChange={e => setTheme(e.currentTarget.value)}>
-              {themes.map(it => <option key={it.value} value={it.value}>{it.name}</option>)}
-            </select>
+        </div>
+        <div className={style.RightMenu}>
+        <div className={style.NetworkSelect}>
+          <div>
+            <a className={style.NetworkSelectL1}>
+                <img src={`${process.env.PUBLIC_URL}/img/ethereum.png`}></img> 
+                <p>ETH</p>
+            </a>
+            <a className={style.NetworkSelectL2}>
+                <img src={`${process.env.PUBLIC_URL}/img/optimism.png`}></img> 
+                <p>OP</p>
+            </a>
           </div>
+        </div>
+        <Web3Connect/>
+        <div className={style.ThemeSelect}>
+          <select value={theme} onChange={e => setTheme(e.currentTarget.value)}>
+                {themes.map(it => <option key={it.value} value={it.value}>{it.name}</option>)}
+          </select>
+        </div>
         </div>
         <div className={style.BlurHeader}></div>
       </header>
