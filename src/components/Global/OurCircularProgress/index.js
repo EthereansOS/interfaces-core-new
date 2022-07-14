687d4da37d11638cec5070b4eb0b083dfabde56c
alet89 <20442827+alet89@users.noreply.github.com>
2022-07-14 20:42:02 +0200

    Loaders improvements

--- src/components/Global/OurCircularProgress/index.js
+++ src/components/Global/OurCircularProgress/index.js
@@ -4,10 +4,14 @@ import style from '../../../all.module.css'
 
 export default ({}) => {
     return (
-    <div className={style.LoaderA}>
-        <div class={style.loadingio_spinner_disk_hn1zpmja0x6}><div class={style.ldio_f2vadqclpalj}>
-                <div><div></div><div></div></div>
+        <><div className={style.LoaderA}>
+            <div class={style.loadingio_spinner_disk_hn1zpmja0x6}><div class={style.ldio_f2vadqclpalj}>
+                    <div><div></div><div></div></div>
+                </div>
             </div>
         </div>
-    </div>)
+        <div className={style.LoaderB}>
+            <img src={`${process.env.PUBLIC_URL}/img/miniloader.gif`}></img>
+        </div>
+        </>)
 }
