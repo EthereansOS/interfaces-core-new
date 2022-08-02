4c77174d9217e17c8b475198f5391557ef90484b
Marco Vasapollo <ceo@metaring.com>
2022-08-02 17:17:04 +0200

    WIP

--- src/components/Global/ObjectsLists/nfts.js
+++ src/components/Global/ObjectsLists/nfts.js
@@ -12,14 +12,14 @@ import LogoRenderer from '../LogoRenderer'
 
 const SingleOpenseaElement = ({element, onClick}) => {
   const context = useEthosContext()
-  const { chainId } = useWeb3()
+  const { chainId, dualChainId } = useWeb3()
   return (
     <a className={style.TokenObject} onClick={() => onClick && onClick(element)}>
       <LogoRenderer badge input={element}/>
       <div className={style.ObjectInfo}>
         <div className={style.ObjectInfoAndLink}>
           <h5>{shortenWord({ context, charsAmount : 15}, element.name)}</h5>
-          <a target="_blank" onClick={e => e.stopPropagation()} href={`https://${chainId === 4 ? 'testnets.' : ''}opensea.io/assets/${element.tokenAddress}/${element.tokenId}`}>Opensea</a>
+          {!dualChainId && <a target="_blank" onClick={e => e.stopPropagation()} href={`https://${chainId === 4 ? 'testnets.' : ''}opensea.io/assets/${element.tokenAddress}/${element.tokenId}`}>Opensea</a>}
           <a className={style.LinkCool}>{element.assetContract.schemaName.split('ERC').join('ERC-')}</a>
         </div>
         <div style={{"visibility" : "visible"}} className={style.ObjectInfoBalance}>
