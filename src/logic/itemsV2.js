a0747dd1370ce67084355e94e733bdbe4bff6f09
Marco Vasapollo <ceo@metaring.com>
2022-08-09 17:08:32 +0200

    WIP

--- src/logic/itemsV2.js
+++ src/logic/itemsV2.js
@@ -142,11 +142,6 @@ export async function loadItemsByFactories(data, factories) {
                     }
                 }
             }, {}))
-            /*l2Tokens = (await Promise.all(l2Tokens.map(async it => {
-                var value = await getRawField({ provider : originalWeb3.currentProvider }, it.l2Address, allMine ? 'balanceOf(address)' : 'totalSupply', account)
-                value = abi.decode(["uint256"], value)[0].toString()
-                return value !== '0' && it
-            }))).filter(it => it)*/
             l2Tokens = l2Tokens.reduce((acc, it) => ({ ...acc, [it.itemId] : it}), {})
             var keys = Object.keys(l2Tokens)
             itemIds = itemIds.filter(it => keys.indexOf(it.itemId) !== -1)
@@ -499,32 +494,56 @@ export async function loadItemDynamicInfo(data, itemData, item) {
     }
 }
 
+function cleanMetadataUris(dataInput, metadata) {
+    var keys = Object.keys(metadata)
+    for(var key of keys) {
+        var value = metadata[key]
+        if(key !== 'description' && value.indexOf && (value.indexOf('://') !== -1 || value.indexOf('//') === 0)) {
+            try {
+                value = cleanUri(dataInput, metadata, value)
+            } catch(e) {
+                console.log('METADATA VALUE', key, value, (e.stack || e.message || e))
+            }
+        }
+        metadata[key] = value
+    }
+    return metadata
+}
+
 export async function loadCollectionMetadata(dataInput, collectionId, mainInterface) {
 
-    var {context, newContract, deep} = dataInput
+    var {context, newContract} = dataInput
 
-    var data = {...await blockchainCall(mainInterface.methods.collection, collectionId)}
-    data.hostContract = newContract(context.MultiOperatorHostABI, data.host)
+    var key = web3Utils.sha3('collection-' + collectionId)
+
+    var metadata = JSON.parse(await cache.getItem(key))
+
+    if(!metadata) {
+        metadata = await tryRetrieveMetadata({context}, {
+            ...(await blockchainCall(mainInterface.methods.collection, collectionId)),
+            mainInterface,
+            id : collectionId
+        })
+        delete metadata.mainInterface
+        metadata = (metadata && cleanMetadataUris(dataInput, metadata)) || metadata
+        metadata && await cache.setItem(key, metadata)
+    }
+
+    metadata.hostContract = newContract(context.MultiOperatorHostABI, metadata.host)
     try {
-        data.mintOperator = await blockchainCall(data.hostContract.methods.operator, 1)
-        data.metadataOperator = await blockchainCall(data.hostContract.methods.operator, 4)
+        metadata.mintOperator = await blockchainCall(metadata.hostContract.methods.operator, 1)
+        metadata.metadataOperator = await blockchainCall(metadata.hostContract.methods.operator, 4)
     } catch(e) {
     }
 
-    var metadata = {
-        ...data,
+    metadata = {
+        ...metadata,
         mainInterface,
-        id : collectionId
+        metadata
     }
-    metadata = !deep ? metadata : await tryRetrieveMetadata({context}, {
-        ...data,
-        mainInterface,
-        id : collectionId
-    })
     if(collectionId === '0xc8ae2302153c696a508f505d7a046ff5fa78dcf79478eea09c682d0101f02252') {
-        metadata.image = 'https://gateway.ipfs.io/ipfs/QmYpYpHVNtvPYJsuDcjfGEXc9y5FozERzQ92JaRcAcfq3h'
+        metadata.image = 'https://ipfs.io/ipfs/QmYpYpHVNtvPYJsuDcjfGEXc9y5FozERzQ92JaRcAcfq3h'
     }
-    var wrappers
     var index = (await loadWrappedCollectionIds(dataInput)).indexOf(collectionId)
 
     metadata = {...metadata, ...itemProjectionsMetadata[index]}
@@ -536,7 +555,7 @@ export async function loadCollection(data, collectionId, factory, item) {
     var collectionData = await loadCollectionMetadata(data, collectionId, item = item || newContract(context.ItemMainInterfaceABI, await blockchainCall(factory.methods.mainInterface)))
     return {
         ...collectionData,
-        items : await loadItemsByFactories({...data, collectionData}, factory)
+        items : await loadItemsByFactories({...data, collectionData, lightweight : false}, factory)
     }
 }
 
