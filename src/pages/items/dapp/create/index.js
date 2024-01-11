9b673bf4aedb9525929190d81da676da3f97b734
Simona Pastore <simona.pastore@beatdata.it>
2024-01-11 15:36:20 +0100

    Partial commit - UI changes in Items>Create section

--- src/pages/items/dapp/create/index.js
+++ src/pages/items/dapp/create/index.js
@@ -3,29 +3,59 @@ import { Link } from 'react-router-dom'
 import style from '../../../../all.module.css'
 
 const Create = ({}) => {
-  return (<>
-        <div className={style.CreatePage}>
-          <div className={style.CreateBoxDesc}>
-          <h6>New Collection</h6>
-            <p>Deploy an on-chain folder to manage and host your Items.</p>
-            <Link className={style.NextStep} to="/items/create/collection">Start</Link>
-            <a target="_blank" className={style.ExtLinkButtonAlpha} href="https://docs.ethos.wiki/ethereansos-docs/organizations/delegations">Learn</a>
-          </div>
-          <div className={style.CreateBoxDesc}>
-          <h6>Manage Collection</h6>
-            <p>Manage Items in any Collection you have permission to host. You can create entirely new Items for the Collection, or mint more of old ones.</p>
-            <Link className={style.NextStep} to="/items/create/item/">Start</Link>
-            <a target="_blank" className={style.ExtLinkButtonAlpha} href="https://docs.ethos.wiki/ethereansos-docs/organizations/delegations">Learn</a>
-          </div>
+  return (
+    <div className={style.MasCardLayer}>
+      <div className={style.MasCard}>
+        <div className={style.MasCardHeader}>
+          <div>New Collection</div>
         </div>
-    </>
+        <div className={style.MasCardBody}>
+          <p>Deploy an on-chain folder to manage and host your Items.</p>
+        </div>
+        <div className={style.MasCardBody}>
+          <Link className={style.NextStep} to="/items/create/collection">
+            Start
+          </Link>
+          <a
+            target="_blank"
+            className={style.ExtLinkButtonAlpha}
+            href="https://docs.ethos.wiki/ethereansos-docs/organizations/delegations">
+            Learn
+          </a>
+        </div>
+      </div>
+
+      <div className={style.MasCard}>
+        <div className={style.MasCardHeader}>
+          <div>Manage Collection</div>
+        </div>
+        <div className={style.MasCardBody}>
+          <p>
+            Manage Items in any Collection you have permission to host. You can
+            create entirely new Items for the Collection, or mint more of old
+            ones.
+          </p>
+        </div>
+        <div className={style.MasCardBody}>
+          <Link className={style.NextStep} to="/items/create/item">
+            Start
+          </Link>
+          <a
+            target="_blank"
+            className={style.ExtLinkButtonAlpha}
+            href="https://docs.ethos.wiki/ethereansos-docs/organizations/delegations">
+            Learn
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
 Create.menuVoice = {
-  label : 'Create',
-  path : '/items/create',
-  contextualRequire : () => require.context('./', false, /.js$/)
+  label: 'Create',
+  path: '/items/create',
+  contextualRequire: () => require.context('./', false, /.js$/),
 }
 
 export default Create
