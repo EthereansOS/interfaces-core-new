a33f1989af2a69299ba4bc79efb943746bd1405f
Simona Pastore <simona.pastore@beatdata.it>
2024-03-08 17:47:13 +0100

    Minor fix for mobile

--- src/pages/factories/dapp/index.js
+++ src/pages/factories/dapp/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 
-
 import style from './factories-main.module.css'
 
 const FactoriesMain = () => {
@@ -8,38 +7,42 @@ const FactoriesMain = () => {
     <>
       <div className={style.ComingSoon}>
         <img src={`${process.env.PUBLIC_URL}/img/Factsoon.png`}></img>
-        <h6>Coming soon. While you wait, you can play with the Factory contracts directly, with the help of the <a target="_blank" href="https://docs.ethos.wiki/ethereansos-docs/">documentation</a></h6>
+        <h6>
+          Coming soon. While you wait, you can play with the Factory contracts
+          directly, with the help of the{' '}
+          <a target="_blank" href="https://docs.ethos.wiki/ethereansos-docs/">
+            documentation
+          </a>
+        </h6>
       </div>
     </>
   )
 }
 
-FactoriesMain.pluginIndex = 40;
+FactoriesMain.pluginIndex = 40
 FactoriesMain.addToPlugin =
-  ({index}) =>
-    ({addElement}) => {
-      addElement('router', {
-        index,
-        path: '/factories',
-        Component: FactoriesMain,
-        exact: true,
-        requireConnection: true,
-        templateProps: {
-          menuName: 'appMenu',
-          isDapp: true,
-          link: '/factories'
-        },
-      })
-
-      addElement('appMenu', {
-        name: 'Launch Factory',
-        label: 'Launch Factory',
+  ({ index }) =>
+  ({ addElement }) => {
+    addElement('router', {
+      index,
+      path: '/factories',
+      Component: FactoriesMain,
+      exact: true,
+      requireConnection: true,
+      templateProps: {
+        menuName: 'appMenu',
+        isDapp: true,
         link: '/factories',
-        index,
-        image : `${process.env.PUBLIC_URL}/img/launchpad.png`,
-      })
-    }
+      },
+    })
 
-
+    addElement('appMenu', {
+      name: 'Launch Factory',
+      label: 'Launch Factory',
+      link: '/factories',
+      index,
+      image: `${process.env.PUBLIC_URL}/img/launchpad.png`,
+    })
+  }
 
 export default FactoriesMain
