1f0c1d8d3c1fc987faf1e6ce261f233502dfe278
Simona Pastore <simona.pastore@beatdata.it>
2024-03-12 17:31:13 +0100

    Minor css fixes

--- src/components/Organizations/ExploreOrganizations/index.js
+++ src/components/Organizations/ExploreOrganizations/index.js
@@ -41,45 +41,71 @@ const ExploreOrganization = ({ address, type }) => {
   }, [])
 
   return (
-    <div className={style.ItemSingle}> 
-    <Link
-     
-      to={`/organizations/${type ? type + '/' : ''}${address}`}>
-      {element ? (
-        <>
-        <button className={style.ItemTitleTopZoneLikeButton}><svg  viewBox="0 0 24 24" fill="none"><path d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z" stroke="currentColor" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></button>
-        <svg className={style.ItemTitleTopZone} viewBox="0 0 196 55" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M196 55V0H0.5V1H4.05286C12.4067 1 20.1595 5.34387 24.5214 12.4685L43.5393 43.5315C47.9012 50.6561 55.654 55 64.0078 55H196Z" fill="currentColor"></path></svg>
-        <div className={style.ItemInfoSide}>
-        {type === 'delegations' ? (
-            <>
-              <p className={style.ItemTitleTopZoneLabel}>Supporters stake</p>
-              <p className={style.ItemTitleTopZoneValue}>3,684.8533 OS</p>
-            </>
-          ) : (
-            <>
-              <p className={style.ItemTitleTopZoneLabel}>Treasury Balance</p>
-              <p className={style.ItemTitleTopZoneValue}>1.292,92$</p>
-            </>
-          )}
-          
-        </div>
-          <LogoRenderer input={element} />
-          <div className={style.ItemTitle}>
-          <h6>{element.name}</h6>
-          {type === 'delegations' ? (
-            <h4>Delegation</h4>
-          ) : (
-            <h4>Organization</h4>
-          )}
-        </div>
-        <svg className={style.ItemTitleBottomZone} width="281" viewBox="0 0 281 99" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 0V99H258.059C248.54 99 239.92 93.3743 236.089 84.6606L205.167 14.3394C201.335 5.62568 192.716 0 183.197 0H0Z" fill="currentColor"></path></svg>
-      
-        </>
-       
-      ) : (
-        <OurCircularProgress />
-      )}
-    </Link>
+    <div className={style.ItemSingle}>
+      <Link to={`/organizations/${type ? type + '/' : ''}${address}`}>
+        {element ? (
+          <>
+            <button className={style.ItemTitleTopZoneLikeButton}>
+              <svg viewBox="0 0 24 24" fill="none">
+                <path
+                  d="M12.62 20.81C12.28 20.93 11.72 20.93 11.38 20.81C8.48 19.82 2 15.69 2 8.68998C2 5.59998 4.49 3.09998 7.56 3.09998C9.38 3.09998 10.99 3.97998 12 5.33998C13.01 3.97998 14.63 3.09998 16.44 3.09998C19.51 3.09998 22 5.59998 22 8.68998C22 15.69 15.52 19.82 12.62 20.81Z"
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"></path>
+              </svg>
+            </button>
+            <svg
+              className={style.ItemTitleTopZone}
+              viewBox="0 0 196 55"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M196 55V0H0.5V1H4.05286C12.4067 1 20.1595 5.34387 24.5214 12.4685L43.5393 43.5315C47.9012 50.6561 55.654 55 64.0078 55H196Z"
+                fill="currentColor"></path>
+            </svg>
+            <div className={style.ItemInfoSide}>
+              {type === 'delegations' ? (
+                <>
+                  <p className={style.ItemTitleTopZoneLabel}>
+                    Supporters stake
+                  </p>
+                  <p className={style.ItemTitleTopZoneValue}>3,684.8533 OS</p>
+                </>
+              ) : (
+                <>
+                  <p className={style.ItemTitleTopZoneLabel}>
+                    Treasury Balance
+                  </p>
+                  <p className={style.ItemTitleTopZoneValue}>1.292,92$</p>
+                </>
+              )}
+            </div>
+            <LogoRenderer input={element} />
+            <div className={style.ItemTitle}>
+              <h6>{element.name}</h6>
+              {type === 'delegations' ? (
+                <h4>Delegation</h4>
+              ) : (
+                <h4>Organization</h4>
+              )}
+            </div>
+            <svg
+              className={style.ItemTitleBottomZone}
+              width="281"
+              viewBox="0 0 281 99"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0 0V99H258.059C248.54 99 239.92 93.3743 236.089 84.6606L205.167 14.3394C201.335 5.62568 192.716 0 183.197 0H0Z"
+                fill="currentColor"></path>
+            </svg>
+          </>
+        ) : (
+          <OurCircularProgress />
+        )}
+      </Link>
     </div>
   )
 }
--- src/pages/governances/dapp/delegations/index.js
+++ src/pages/governances/dapp/delegations/index.js
@@ -24,52 +24,99 @@ const DelegationsList = ({ mine, onList }) => {
   const { getGlobalContract, newContract, chainId, account, dualChainId } =
     useWeb3()
 
-  return (<div className={style.SectionMinWidth}>
-  {/*<Banners bannerA="banner1" bannerB="banner2" sizeA="36%" sizeB="54%" titleA="Rule Together" titleB="Be a Player In the Game of Organizations" linkA="https://docs.ethos.wiki/ethereansos-docs/guilds/guilds-documentation/delegations/how-delegations-work" linkB="https://docs.ethos.wiki/ethereansos-docs/guilds/guilds-documentation/delegations" textA="Delegations are independent political parties that compete with each other for grant funding from one or more EthOS Organizations." textB="Create and lead a Delegation. Rally the support of an EthOS Organization’s token holders to win grant funding. Use that funding to govern your Delegation’s political economy with your supporters."/>*/}
-  <ul className={style.SectionSubMenuItems}>
-      <li><Link to="/organizations">Organizations</Link></li>
-      <li className={style.SectionSubMenuItemsActive}><Link to="/organizations/delegations">Delegations</Link></li>
-    </ul>
-    <div className={style.ItemsExploreMainTitleArea}>
-      <h2>Delegations
-       
-      </h2>
-      <Link to="/organizations/create/delegation" className={style.ItemsExploreMainCategoriesCreateElement} style={{'marginTop': '10px'}}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9 10C10.1046 10 11 9.10457 11 8C11 6.89543 10.1046 6 9 6C7.89543 6 7 6.89543 7 8C7 9.10457 7.89543 10 9 10Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2.67004 18.9501L7.60004 15.6401C8.39004 15.1101 9.53004 15.1701 10.24 15.7801L10.57 16.0701C11.35 16.7401 12.61 16.7401 13.39 16.0701L17.55 12.5001C18.33 11.8301 19.59 11.8301 20.37 12.5001L22 13.9001" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+  return (
+    <div className={style.SectionMinWidth}>
+      {/*<Banners bannerA="banner1" bannerB="banner2" sizeA="36%" sizeB="54%" titleA="Rule Together" titleB="Be a Player In the Game of Organizations" linkA="https://docs.ethos.wiki/ethereansos-docs/guilds/guilds-documentation/delegations/how-delegations-work" linkB="https://docs.ethos.wiki/ethereansos-docs/guilds/guilds-documentation/delegations" textA="Delegations are independent political parties that compete with each other for grant funding from one or more EthOS Organizations." textB="Create and lead a Delegation. Rally the support of an EthOS Organization’s token holders to win grant funding. Use that funding to govern your Delegation’s political economy with your supporters."/>*/}
+      <ul className={style.SectionSubMenuItems}>
+        <li>
+          <Link to="/organizations">Organizations</Link>
+        </li>
+        <li className={style.SectionSubMenuItemsActive}>
+          <Link to="/organizations/delegations">Delegations</Link>
+        </li>
+      </ul>
+      <div className={style.ItemsExploreMainTitleArea}>
+        <h2>Delegations</h2>
+        <Link
+          to="/organizations/create/delegation"
+          className={style.ItemsExploreMainCategoriesCreateElement}
+          style={{ marginTop: '10px' }}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>
+            <path
+              d="M9 10C10.1046 10 11 9.10457 11 8C11 6.89543 10.1046 6 9 6C7.89543 6 7 6.89543 7 8C7 9.10457 7.89543 10 9 10Z"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>
+            <path
+              d="M2.67004 18.9501L7.60004 15.6401C8.39004 15.1101 9.53004 15.1701 10.24 15.7801L10.57 16.0701C11.35 16.7401 12.61 16.7401 13.39 16.0701L17.55 12.5001C18.33 11.8301 19.59 11.8301 20.37 12.5001L22 13.9001"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>
+          </svg>
           <span>Create</span>
         </Link>
-      <p>Dynamic SubDAOs to Empower Organizations</p>
-      <div className={style.ItemsExploreMainSearch} style={{'marginRight': '140px'}}>
-        <input placeholder="Search delegations..." />
-        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"></path>
-          <path
-            d="M22 22L20 20"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"></path>
-        </svg>
+        <p>Dynamic SubDAOs to Empower Organizations</p>
+        <div
+          className={style.ItemsExploreMainSearch}
+          style={{ marginRight: '140px' }}>
+          <input placeholder="Search delegations..." />
+          <svg
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>
+            <path
+              d="M22 22L20 20"
+              stroke="currentColor"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"></path>
+          </svg>
+        </div>
+      </div>
+      <div className={style.OrganizationsExploreMain}>
+        <Web3DependantList
+          Renderer={ExploreDelegations}
+          rendererIsContainer
+          emptyMessage={mine ? '' : undefined}
+          discriminant={mine && account}
+          provider={() =>
+            list ||
+            all({
+              context,
+              getGlobalContract,
+              newContract,
+              chainId,
+              account,
+              mine,
+              factoryOfFactories: getGlobalContract('factoryOfFactories'),
+            }).then((l) => {
+              setList(l)
+              return l
+            })
+          }
+        />
       </div>
     </div>
-  <div className={style.OrganizationsExploreMain}>
-    <Web3DependantList
-      Renderer={ExploreDelegations}
-      rendererIsContainer
-      emptyMessage={mine ? "" : undefined}
-      discriminant={mine && account}
-      provider={() => list || all({context, getGlobalContract, newContract, chainId, account, mine, factoryOfFactories : getGlobalContract('factoryOfFactories')}).then(l => {
-        setList(l)
-        return l
-      }) }
-    />
-  </div>
-  </div>)
+  )
 }
 
 DelegationsList.menuVoice = {
