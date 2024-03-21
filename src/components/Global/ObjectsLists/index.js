578354218ab54d9d628b93f18308b06f4702e7f9
Simona Pastore <simona.pastore@beatdata.it>
2024-03-21 13:18:05 +0100

    Restored items for voting tokens

--- src/components/Global/ObjectsLists/index.js
+++ src/components/Global/ObjectsLists/index.js
@@ -1,40 +1,55 @@
 import React, { useState } from 'react'
 
-import ERC20TokenObject  from './erc20-token-object.js'
-import ItemObject  from './item-object.js'
-import CategoryObject  from './category-object.js'
+import ERC20TokenObject from './erc20-token-object.js'
+import ItemObject from './item-object.js'
+import CategoryObject from './category-object.js'
 import DefaultChild from './child'
 import NFTS from './nfts'
 
 import style from '../../../all.module.css'
 
-var defaultSelections = [{
-  name : 'ERC-20',
-  value : ERC20TokenObject
-}, {
-  name : 'Items',
-  value : ItemObject
-},/* {
+var defaultSelections = [
+  {
+    name: 'ERC-20',
+    value: ERC20TokenObject,
+  },
+  {
+    name: 'Items',
+    value: ItemObject,
+  },
+  /* {
   name : 'Items V1',
   value : ItemObject,
   properties : {hardCabledList : 'itemsV1ListURL'}
 },*/ {
-  name : 'Collections',
-  value : CategoryObject
-}, {
-  name : 'ERC-721',
-  value : NFTS,
-  properties : {type : 'ERC721'}
-}, {
-  name : 'ERC-1155',
-  value : NFTS,
-  properties : {type : 'ERC1155'}
-}];
+    name: 'Collections',
+    value: CategoryObject,
+  },
+  {
+    name: 'ERC-721',
+    value: NFTS,
+    properties: { type: 'ERC721' },
+  },
+  {
+    name: 'ERC-1155',
+    value: NFTS,
+    properties: { type: 'ERC1155' },
+  },
+]
 
-const ObjectsLists = ({onlySelections, selectionProperties, list}) => {
-  onlySelections = onlySelections && onlySelections instanceof Array ? onlySelections : [onlySelections]
-  var selections = onlySelections ? defaultSelections.filter(it => onlySelections.indexOf(it.name) !== -1) : defaultSelections
-  selectionProperties && (selections = selections.map(it => ({...it, properties : {...it.properties, ...selectionProperties[it.name]}})))
+const ObjectsLists = ({ onlySelections, selectionProperties, list }) => {
+  onlySelections =
+    onlySelections && onlySelections instanceof Array
+      ? onlySelections
+      : [onlySelections]
+  var selections = onlySelections
+    ? defaultSelections.filter((it) => onlySelections.indexOf(it.name) !== -1)
+    : defaultSelections
+  selectionProperties &&
+    (selections = selections.map((it) => ({
+      ...it,
+      properties: { ...it.properties, ...selectionProperties[it.name] },
+    })))
 
   var [currentSelection, setCurrentSelection] = useState(selections[0])
   var [searchText, setSearchText] = useState('')
@@ -44,14 +59,34 @@ const ObjectsLists = ({onlySelections, selectionProperties, list}) => {
     <div className={style.TokensSelector}>
       <div className={style.TokensSelectorList}>
         <div className={style.ObjectsLists}>
-          <Child {...{list, searchText, ...currentSelection.properties}}/>
+          <Child {...{ list, searchText, ...currentSelection.properties }} />
         </div>
       </div>
       <div className={style.TokensSelectorCategories}>
-        <input type="text" placeholder="Search name or address" value={searchText} onChange={e => setSearchText(e.currentTarget.value)}/>
-        {!list && <div className={style.TokensSelectorCategoriesList}>
-          {selections.map(it => <a key={it.name} className={currentSelection === it ? style.selected : undefined} onClick={() => setCurrentSelection(it)}>{it.name}</a>)}
-        </div>}
+        <input
+          type="text"
+          placeholder="Search name or address"
+          value={searchText}
+          onChange={(e) => setSearchText(e.currentTarget.value)}
+        />
+        {!list && (
+          <div className={style.TokensSelectorCategoriesList}>
+            {selections.map((it) => {
+              return (
+                <a
+                  key={it.name}
+                  className={
+                    currentSelection.name === it.name
+                      ? style.selected
+                      : undefined
+                  }
+                  onClick={() => setCurrentSelection(it)}>
+                  {it.name}
+                </a>
+              )
+            })}
+          </div>
+        )}
       </div>
     </div>
   )
--- src/components/Global/TokenInputRegular/index.js
+++ src/components/Global/TokenInputRegular/index.js
@@ -2,101 +2,197 @@ import React, { useState, useEffect, useCallback, useMemo } from 'react'
 
 import style from '../../../all.module.css'
 import CircularProgress from '../OurCircularProgress'
-import { abi, blockchainCall, useWeb3, fromDecimals, toDecimals, shortenWord, useEthosContext, formatMoney, numberToString } from 'interfaces-core'
+import {
+  abi,
+  blockchainCall,
+  useWeb3,
+  fromDecimals,
+  toDecimals,
+  shortenWord,
+  useEthosContext,
+  formatMoney,
+  numberToString,
+} from 'interfaces-core'
 import ModalStandard from '../ModalStandard'
 import ObjectsLists from '../ObjectsLists'
 import LogoRenderer from '../LogoRenderer'
 import { getRawField } from '../../../logic/generalReader'
 
-function numberInputOnWheelPreventChange (e) {
-    const target = e.currentTarget
-    target.blur()
-    e.stopPropagation()
-    setTimeout(target.focus)
+function numberInputOnWheelPreventChange(e) {
+  const target = e.currentTarget
+  target.blur()
+  e.stopPropagation()
+  setTimeout(target.focus)
+}
+
+const TokenInputRegular = ({
+  onElement,
+  onlySelections,
+  tokens,
+  tokenOnly,
+  noETH,
+  selected,
+  outputValue,
+  disabled,
+  noBalance,
+  onImageClick,
+}) => {
+  const context = useEthosContext()
+
+  const { block, account, web3 } = useWeb3()
+
+  const element = tokens && tokens.length === 1 ? tokens[0] : selected || null
+
+  const [balance, setBalance] = useState(null)
+
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  const [value, setValue] = useState(outputValue)
+
+  useEffect(() => {
+    outputValue && setValue(outputValue)
+  }, [outputValue])
+
+  useEffect(() => {
+    !element && setBalance(null)
+    if (noBalance || tokenOnly) {
+      return setBalance(null)
+    }
+    element &&
+      element.contract &&
+      !element.mainInterface &&
+      blockchainCall(element.contract.methods.balanceOf, account).then(
+        setBalance
+      )
+    element &&
+      element.id &&
+      element.contract &&
+      element.mainInterface &&
+      !element.l2Address &&
+      blockchainCall(
+        element.contract.methods.balanceOf,
+        account,
+        element.id
+      ).then(setBalance)
+    element &&
+      element.contract &&
+      element.mainInterface &&
+      element.l2Address &&
+      getRawField(
+        { provider: web3.currentProvider },
+        element.l2Address,
+        'balanceOf(address)',
+        account
+      ).then((it) => setBalance(abi.decode(['uint256'], it)[0].toString()))
+  }, [account, element, block, noBalance])
+
+  useEffect(() => {
+    onElement &&
+      onElement(
+        element,
+        balance || '0',
+        (!element
+          ? ''
+          : !value || value === '0'
+          ? ''
+          : toDecimals(value, element.decimals)) || ''
+      )
+  }, [element, balance, value])
+
+  var onClick = (el) => {
+    onElement && onElement(el, '0', '0')
+    setValue('0')
+    setModalIsOpen(false)
   }
 
-const TokenInputRegular = ({onElement, onlySelections, tokens, tokenOnly, noETH, selected, outputValue, disabled, noBalance, onImageClick}) => {
+  const selections = onlySelections || ['ERC-20', 'Items']
+  const properties = selections.reduce(
+    (acc, it) => ({
+      ...acc,
+      [it]: {
+        noETH: noETH === true,
+        renderedProperties: { onClick, noBalance },
+      },
+    }),
+    {}
+  )
 
-    const context = useEthosContext()
-
-    const { block, account, web3 } = useWeb3()
-
-    const element = tokens && tokens.length === 1 ? tokens[0] : selected || null
-
-    const [balance, setBalance] = useState(null)
-
-    const [modalIsOpen, setModalIsOpen] = useState(false)
-
-    const [value, setValue] = useState(outputValue)
-
-    useEffect(() => {
-        outputValue && setValue(outputValue)
-    }, [outputValue])
-
-    useEffect(() => {
-        !element && setBalance(null)
-        if(noBalance || tokenOnly) {
-            return setBalance(null)
-        }
-        element && element.contract && !element.mainInterface && blockchainCall(element.contract.methods.balanceOf, account).then(setBalance)
-        element && element.id && element.contract && element.mainInterface && !element.l2Address && blockchainCall(element.contract.methods.balanceOf, account, element.id).then(setBalance)
-        element && element.contract && element.mainInterface && element.l2Address && getRawField({ provider : web3.currentProvider}, element.l2Address, 'balanceOf(address)', account).then(it => setBalance(abi.decode(['uint256'], it)[0].toString()))
-    }, [account, element, block, noBalance])
-
-    useEffect(() => {
-        onElement && onElement(element, balance || '0', (!element ? '' : !value || value === '0' ? '' : toDecimals(value, element.decimals)) || '')
-    }, [element, balance, value])
-
-    var onClick = el => {
-        onElement && onElement(el, '0', '0')
-        setValue('0')
-        setModalIsOpen(false)
+  const onValueChange = useCallback((e) => {
+    var v = e.currentTarget.value
+    v = v.indexOf('.') === 0 && v !== '.' ? '0' + v : v
+    if (v !== '0.' && !v.endsWith('0')) {
+      v = numberToString(parseFloat(v))
     }
+    setValue(v)
+  }, [])
 
-    const selections = onlySelections || ['ERC-20', 'Items']
-    const properties = selections.reduce((acc, it) => ({...acc, [it] : {noETH : noETH === true, renderedProperties:{onClick, noBalance}}}), {})
+  const valueInput = useMemo(() => {
+    if (
+      !element ||
+      isNaN(parseFloat(value)) ||
+      (parseFloat(value) === 0 && value === '0')
+    ) {
+      return ''
+    }
+    return value
+  }, [value, element])
 
-    const onValueChange = useCallback(e => {
-        var v = e.currentTarget.value
-        v = v.indexOf('.') === 0 && v !== '.' ? ('0' + v) : v
-        if(v !== '0.' && !v.endsWith('0')) {
-            v = numberToString(parseFloat(v))
-        }
-        setValue(v)
-    }, [])
-
-    const valueInput = useMemo(() => {
-        if(!element || isNaN(parseFloat(value)) || (parseFloat(value) === 0 && value === '0')) {
-            return ''
-        }
-        return value
-    }, [value, element])
-
-    return modalIsOpen ? (
-        <ModalStandard close={() => setModalIsOpen(false)}>
-            <ObjectsLists
-                list={tokens}
-                onlySelections={selections}
-                selectionProperties={properties}
+  return modalIsOpen ? (
+    <ModalStandard close={() => setModalIsOpen(false)}>
+      <ObjectsLists
+        list={tokens}
+        onlySelections={selections}
+        selectionProperties={properties}
+      />
+    </ModalStandard>
+  ) : (
+    <div className={style.TradeMarketTokenAll}>
+      <div className={style.TradeMarketToken}>
+        <a
+          onClick={() =>
+            onImageClick
+              ? onImageClick()
+              : (!tokens || tokens.length > 1) && setModalIsOpen(true)
+          }
+          className={style.TradeMarketTokenSelector}>
+          <LogoRenderer input={element} />
+          <span>
+            {shortenWord({ context, charsAmount: 15 }, element?.symbol || '')}
+            {!tokens || tokens.length > 1 ? <span> ▼</span> : ''}
+          </span>
+        </a>
+        {!tokenOnly && (
+          <div className={style.TradeMarketTokenAmount}>
+            <input
+              onWheel={numberInputOnWheelPreventChange}
+              disabled={disabled}
+              type="number"
+              placeholder="0.0"
+              min="0.000000000000000001"
+              step="0.001"
+              value={valueInput}
+              onChange={onValueChange}
             />
-        </ModalStandard>
-    ) : (
-        <div className={style.TradeMarketTokenAll}>
-            <div className={style.TradeMarketToken}>
-                <a onClick={() => onImageClick ? onImageClick() : (!tokens || tokens.length > 1) && setModalIsOpen(true)} className={style.TradeMarketTokenSelector}>
-                    <LogoRenderer input={element}/>
-                    <span>{shortenWord({ context, charsAmount : 15}, element?.symbol || '')}{(!tokens || tokens.length > 1) ? <span> ▼</span> : ''}</span>
-                </a>
-                {!tokenOnly && <div className={style.TradeMarketTokenAmount}>
-                    <input onWheel={numberInputOnWheelPreventChange} disabled={disabled} type="number" placeholder="0.0" min="0.000000000000000001" step="0.001" value={valueInput} onChange={onValueChange}/>
-                </div>}
-            </div>
-            {!noBalance && !tokenOnly && element && balance === null && <CircularProgress/>}
-            {(element && balance !== null && !noBalance && !tokenOnly) ? <a onClick={() => !disabled && setValue(fromDecimals(balance, element.decimals, true))} className={style.TradeMarketTokenBalance}>
-                Balance: {formatMoney(fromDecimals(balance, element.decimals, true))} {shortenWord({ context, charsAmount : 15}, element.symbol)}
-            </a> : <a className={style.TradeMarketTokenBalance}>{'\u00a0'}</a>}
-        </div>
-    )
+          </div>
+        )}
+      </div>
+      {!noBalance && !tokenOnly && element && balance === null && (
+        <CircularProgress />
+      )}
+      {element && balance !== null && !noBalance && !tokenOnly ? (
+        <a
+          onClick={() =>
+            !disabled && setValue(fromDecimals(balance, element.decimals, true))
+          }
+          className={style.TradeMarketTokenBalance}>
+          Balance: {formatMoney(fromDecimals(balance, element.decimals, true))}{' '}
+          {shortenWord({ context, charsAmount: 15 }, element.symbol)}
+        </a>
+      ) : (
+        <a className={style.TradeMarketTokenBalance}>{'\u00a0'}</a>
+      )}
+    </div>
+  )
 }
 
 export default TokenInputRegular
