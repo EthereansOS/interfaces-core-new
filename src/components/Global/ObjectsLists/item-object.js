879dc761ba23611ae3035bdefb3b0645da7317ae
Simona Pastore <simona.pastore@beatdata.it>
2024-04-02 14:15:05 +0200

    Fixed item search

--- src/components/Global/ObjectsLists/item-object.js
+++ src/components/Global/ObjectsLists/item-object.js
@@ -8,8 +8,19 @@ import ItemObjectElement from './element/item-object-element'
 import Web3DependantList from '../Web3DependantList'
 import { useOpenSea } from '../../../logic/uiUtilities'
 
-export default ({onResult, provider, discriminant, allMine, forCollection, excluding, element = ItemObjectElement, wrappedOnly, renderedProperties, hardCabledList, searchText}) => {
-
+export default ({
+  onResult,
+  provider,
+  discriminant,
+  allMine,
+  forCollection,
+  excluding,
+  element = ItemObjectElement,
+  wrappedOnly,
+  renderedProperties,
+  hardCabledList,
+  searchText,
+}) => {
   const context = useEthosContext()
 
   const web3Data = useWeb3()
@@ -18,11 +29,11 @@ export default ({onResult, provider, discriminant, allMine, forCollection, exclu
 
   const seaport = useOpenSea()
 
-  const sortOrder = function(a, b) {
-    if(parseInt(a.totalSupply) > parseInt(b.totalSupply)) {
+  const sortOrder = function (a, b) {
+    if (parseInt(a.totalSupply) > parseInt(b.totalSupply)) {
       return -1
     }
-    if(parseInt(a.totalSupply) < parseInt(b.totalSupply)) {
+    if (parseInt(a.totalSupply) < parseInt(b.totalSupply)) {
       return 1
     }
     return 0
@@ -31,22 +42,95 @@ export default ({onResult, provider, discriminant, allMine, forCollection, exclu
   const tokenAddress = useMemo(() => {
     try {
       return web3Utils.toChecksumAddress(searchText)
-    } catch(e) {
-    }
+    } catch (e) {}
   }, [searchText])
 
-  const filter = useCallback(it => !tokenAddress && searchText ? it.name?.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 || it.symbol?.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 : it, [tokenAddress, searchText])
+  const filter = useCallback(
+    (it) =>
+      !tokenAddress && searchText
+        ? it.name?.toLowerCase().indexOf(searchText.toLowerCase()) !== -1 ||
+          it.symbol?.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+        : it,
+    [tokenAddress, searchText]
+  )
 
-  return <Web3DependantList
-    Renderer={element}
-    renderedProperties={{...renderedProperties, wrappedOnly, allMine}}
-    provider={() => (tokenAddress ? loadTokenFromAddress({ context, ...web3Data, forceItem : true }, tokenAddress) : provider ? provider() : hardCabledList ? loadTokens({context, chainId, web3, account, newContract, alsoETH : false, listName : hardCabledList}) : loadItemsByFactories({seaport, context, ...web3Data, collectionData : forCollection, excluding, wrappedOnly, allMine}, getGlobalContract("itemProjectionFactory"))).then(r => {
-      onResult && onResult(r)
-      return r
-    }) }
-    sortOrder={sortOrder}
-    emptyMessage={tokenAddress ? `No Item found for address ${tokenAddress}` : ''}
-    discriminant={discriminant || tokenAddress ? tokenAddress : allMine ? account : hardCabledList}
-    filter={filter}
-  />
+  return (
+    <Web3DependantList
+      Renderer={element}
+      renderedProperties={{ ...renderedProperties, wrappedOnly, allMine }}
+      provider={() =>
+        (tokenAddress
+          ? loadTokenFromAddress(
+              { context, ...web3Data, forceItem: true },
+              tokenAddress
+            )
+          : provider
+          ? provider()
+          : hardCabledList
+          ? loadTokens({
+              context,
+              chainId,
+              web3,
+              account,
+              newContract,
+              alsoETH: false,
+              listName: hardCabledList,
+            })
+          : loadItemsByFactories(
+              {
+                seaport,
+                context,
+                ...web3Data,
+                collectionData: forCollection,
+                excluding,
+                wrappedOnly,
+                allMine,
+              },
+              getGlobalContract('itemProjectionFactory')
+            )
+        ).then(async (r) => {
+          var itemsList = []
+
+          if (r) {
+            for (let i = 0; i < r.length; i++) {
+              let it = r[i]
+              var address = it.itemId
+              try {
+                address =
+                  address.toLowerCase().indexOf('0x') === 0
+                    ? address
+                    : web3Utils.numberToHex(address)
+                var itemProperties = await loadTokenFromAddress(
+                  { context, ...web3Data, seaport },
+                  address
+                )
+                if (itemProperties) {
+                  it.name = itemProperties.name
+                  it.symbol = itemProperties.symbol
+                }
+              } catch (e) {}
+
+              itemsList.push(it)
+            }
+          }
+
+          onResult && onResult(itemsList)
+          return r
+        })
+      }
+      searchText={tokenAddress ? '' : searchText}
+      sortOrder={sortOrder}
+      emptyMessage={
+        tokenAddress ? `No Item found for address ${tokenAddress}` : ''
+      }
+      discriminant={
+        discriminant || tokenAddress
+          ? tokenAddress
+          : allMine
+          ? account
+          : hardCabledList
+      }
+      filter={filter}
+    />
+  )
 }
